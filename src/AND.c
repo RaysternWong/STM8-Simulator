b433a28c705b02e15256b3e536a3331cd40b1ea5
Raydivine <radivine00@gmail.com>
2016-06-17 11:06:26 +0800

    added AND files

---
+++ src/AND.c
@@ -0,0 +1,85 @@
+#include "AND.h"
+#include <stdio.h>
+#include <stdint.h>
+
+/**
+*  Please refer the folowing instruction in the page 77 of programming manual
+*
+*   'a'  means accumulator
+*   'ht' means hast tag , #
+*/
+
+uint8_t and_a_ht_byte(uint8_t *opcode){
+  opcode++;
+  return 2;
+}
+
+uint8_t and_a_shortmem(uint8_t *opcode){
+  opcode++;
+  return 2;
+}
+
+uint8_t and_a_longmem(uint8_t *opcode){
+  opcode++;
+  return 3;
+}
+
+uint8_t and_a_x(uint8_t *opcode){
+  opcode++;
+  return 1;
+}
+
+uint8_t and_a_shortoff_x(uint8_t *opcode){
+  opcode++;
+  return 2;
+}
+
+uint8_t and_a_longoff_x(uint8_t *opcode){
+  opcode++;
+  return 3;
+}
+
+uint8_t and_a_y(uint8_t *opcode){
+  opcode++;
+  return 2;
+}
+
+uint8_t and_a_shortoff_y(uint8_t *opcode){
+  opcode++;
+  return 3;
+}
+
+uint8_t and_a_longoff_y(uint8_t *opcode){
+  opcode++;
+  return 4;
+}
+
+uint8_t and_a_shortoff_sp(uint8_t *opcode){
+  opcode++;
+  return 2;
+}
+
+uint8_t and_a_shortptr_w(uint8_t *opcode){
+  opcode++;
+  return 3;
+}
+
+uint8_t and_a_longptr_w(uint8_t *opcode){
+  opcode++;
+  return 4;
+}
+
+uint8_t and_a_shortptr_w_x(uint8_t *opcode){
+  opcode++;
+  return 3;
+}
+
+uint8_t and_a_longptr_w_x(uint8_t *opcode){
+  opcode++;
+  return 4;
+}
+
+uint8_t and_a_shortptr_w_y(uint8_t *opcode){
+  opcode++;
+  return 3;
+}
---
+++ src/AND.h
@@ -0,0 +1,22 @@
+#ifndef AND_H
+#define AND_H
+#include <stdint.h>
+
+uint8_t and_a_ht_byte     (uint8_t *opcode);
+uint8_t and_a_shortmem    (uint8_t *opcode);
+uint8_t and_a_longmem     (uint8_t *opcode);
+uint8_t and_a_x           (uint8_t *opcode);
+uint8_t and_a_shortoff_x  (uint8_t *opcode);
+uint8_t and_a_longoff_x   (uint8_t *opcode);
+uint8_t and_a_y           (uint8_t *opcode);
+uint8_t and_a_shortoff_y  (uint8_t *opcode);
+uint8_t and_a_longoff_y   (uint8_t *opcode);
+uint8_t and_a_shortoff_sp (uint8_t *opcode);
+uint8_t and_a_shortptr_w  (uint8_t *opcode);
+uint8_t and_a_longptr_w   (uint8_t *opcode);
+uint8_t and_a_shortptr_w_x(uint8_t *opcode);
+uint8_t and_a_longptr_w_x (uint8_t *opcode);
+uint8_t and_a_shortptr_w_y(uint8_t *opcode);
+
+
+#endif // AND_H
