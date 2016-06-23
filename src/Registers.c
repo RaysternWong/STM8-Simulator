295997fe9abf9a2a499a079185a9089e1b90c5ae
Raydivine <raydivine00@gmail.com>
2016-06-23 15:55:15 +0800

    Deleted redundant files

--- src/Registers.c
+++
@@ -1,1 +0,0 @@
-#include "Registers.h"
--- test/test_InstructionTable/test_ADD.c
+++ test/test_InstructionTable/test_ADD.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include "CPUConfig.h"
 
+#define A   cpu.accA //Accumulator
 
 void setUp(void)
 {
@@ -15,6 +16,7 @@ void tearDown(void)
 
 
 
+// 0x01 + 0x05 = 0x06
 void test_add_a_ht_byte_with_offset_5(void)
 {
   uint8_t instr[] = {0XAB, 0X05};
