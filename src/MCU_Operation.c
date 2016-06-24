6ac1269ec3533e627703bb98a8780435a2e984d1
Raydivine <radivine00@gmail.com>
2016-06-24 11:51:04 +0800

    5 test for mcu_addw()

--- src/MCU_Operation.c
+++ src/MCU_Operation.c
@@ -131,10 +131,13 @@ void mcu_adc(uint8_t value)
 }
 
 void mcu_addw(uint16_t *reg, uint16_t value){
-  uint16_t dst    = *reg;
-  uint16_t result = dst + value;
+  uint16_t a    = *reg;
+  uint16_t result = a + value;
   *reg = result;
 
-  cpu.ccR.Z = (result == 0 ? 1 : 0);
+  cpu.ccR.V = ( A15 & M15 | M15 & _R15 | _R15 & A15 ) ^ ( A14 & M14 | M14 & _R14 | _R14 & A14 );
+  cpu.ccR.H = A7 & M7 | M7 & _R7 | _R7 & A7;
   cpu.ccR.N = R15;
+  cpu.ccR.Z = (result == 0 ? 1 : 0);
+  cpu.ccR.C = A15 & M15 | M15 & _R15 | _R15 & A15;
 }
--- test/test_MCU_Operation/test_mcu_addw.c
+++ test/test_MCU_Operation/test_mcu_addw.c
@@ -71,99 +71,22 @@ void test_mcu_addw_given_X_equal_neg1_than_add_0_should_get_Xequal_neg1_and_neg_
   TEST_ASSERT_EQUAL(0, CC.C);
 }
 
-/** 0x0F + 0x0F = 0x1E, H = 1 
+/** 0xFF80 + 0x0080 = 0x00, H = 1, C = 1 
 *
-*        0000   1111     0F
-*   +    0000   1111     0F
-*--------------------
-*        0001   1110     1E
-*---------------------
+*        1111   1111  1000  0000   0xFF80
+*   +    0000   0000  1000  0000   0x0080
+*--------------------------------
+*    1   0000   0000  0000  0000
+*--------------------------------
 *
-*  half carry flag is set to 1, because that is carry from bit 3 to bit 4
-*/
-void xtest_mcu_addw_given_a_equal_F_than_add_F_should_get_A_equal_1E_and_half_carry_flag_1(void){
-  A = 0x0F;
- // mcu_addw(0x0F);
-  
-  TEST_ASSERT_EQUAL_INT8(0x1E, A);
-
-  TEST_ASSERT_EQUAL(0, CC.V);
-  TEST_ASSERT_EQUAL(0, CC.l1);
-  TEST_ASSERT_EQUAL(1, CC.H);
-  TEST_ASSERT_EQUAL(0, CC.l0);
-  TEST_ASSERT_EQUAL(0, CC.N);
-  TEST_ASSERT_EQUAL(0, CC.Z);
-  TEST_ASSERT_EQUAL(0, CC.C);
-}
-
-/** 0x0F + 0x01 = 0x10, H = 1 
-*
-*        0000   1111     0F
-*   +    0000   0001     01
-*--------------------
-*        0001   0000     10
-*---------------------
-*
-*  half carry flag is set to 1, because that is carry from bit 3 to bit 4
-*/
-void xtest_mcu_addw_given_a_equal_F_than_add_1_should_get_A_equal_10_and_half_carry_flag_1(void){
-  A = 0x0F;
- // mcu_addw(0x01);
-  
-  TEST_ASSERT_EQUAL_INT8(0x10, A);
-
-  TEST_ASSERT_EQUAL(0, CC.V);
-  TEST_ASSERT_EQUAL(0, CC.l1);
-  TEST_ASSERT_EQUAL(1, CC.H);
-  TEST_ASSERT_EQUAL(0, CC.l0);
-  TEST_ASSERT_EQUAL(0, CC.N);
-  TEST_ASSERT_EQUAL(0, CC.Z);
-  TEST_ASSERT_EQUAL(0, CC.C);
-}
-
-/** 0xF0 + 0xF0 = 0xE0, C = 1 , N = 1
-*
-*        1111   0000    F0
-*   +    1111   0000    F0
-*--------------------
-*    1   1110   0000    E0
-*---------------------
-*
-*  carry flag is set to 1, because that is carry from bit 7 to bit 8
-*  negative flag is set to 1, because R7 is 1
-*/
-void xtest_mcu_addw_given_a_equal_F0_than_add_F0_should_get_A_equal_E0_and_carry_flag_1_neg_flag_1(void){
-  A = 0xF0;
-  //mcu_addw(0xF0);
-  
-  TEST_ASSERT_EQUAL_INT8(0xE0, A);
-
-  TEST_ASSERT_EQUAL(0, CC.V);
-  TEST_ASSERT_EQUAL(0, CC.l1);
-  TEST_ASSERT_EQUAL(0, CC.H);
-  TEST_ASSERT_EQUAL(0, CC.l0);
-  TEST_ASSERT_EQUAL(1, CC.N);
-  TEST_ASSERT_EQUAL(0, CC.Z);
-  TEST_ASSERT_EQUAL(1, CC.C);
-}
-
-/** 0xF8 + 0x08 = 0x00, H = 1, C = 1 
-*
-*        1111   1000    F8
-*   +    0000   1000    08
-*--------------------
-*    1   0000   0000    00
-*---------------------
-*
-*  half carry flag is set to 1, because that is carry from bit 3 to bit 4
-*  carry flag is set to 1, because that is carry from bit 7 to bit 8
+*  half carry flag is set to 1, because that is carry from bit 7 to bit 8
+*  carry flag is set to 1, because that is carry from bit 15 to bit 16
 *  zero  flag is set to 1
 */
-void xtest_mcu_addw_given_a_equal_F8_than_add_08_should_get_A_equal_00_and_half_carry_flag_1_carry_flag_1_zero_flag_1(void){
-  A = 0xF8;
- // mcu_addw(0x08);
-  
-  TEST_ASSERT_EQUAL_INT8(0x00, A);
+void test_mcu_addw_given_x_equal_FF80_than_add_0080_should_get_x_equal_00_and_half_carry_flag_1_carry_flag_1_zero_flag_1(void){
+  X = 0xFF80;
+  mcu_addw(&X, 0x0080);
+  TEST_ASSERT_EQUAL_INT8(0x0000, X);
 
   TEST_ASSERT_EQUAL(0, CC.V);
   TEST_ASSERT_EQUAL(0, CC.l1);
@@ -174,7 +97,7 @@ void xtest_mcu_addw_given_a_equal_F8_than_add_08_should_get_A_equal_00_and_half_
   TEST_ASSERT_EQUAL(1, CC.C);
 }
 
-/** 0x40 + 0x40 = 0x80 , V = 1, N = 1
+/** 0x4000 + 0x4000 = 0x8000 , V = 1, N = 1
 *
 *   In math arithmatic rule
 *   1. Adding two positive numbers must give a positive result
@@ -185,10 +108,10 @@ void xtest_mcu_addw_given_a_equal_F8_than_add_08_should_get_A_equal_00_and_half_
 *    (+A) + (+B) = −C
 *    (−A) + (−B) = +C
 *
-*        0100   0000    40  (+Ve)
-*   +    0100   0000    40  (+Ve)
+*        0100   0000  0000  0000  0x4000  (+Ve)
+*   +    0100   0000  0000  0000  0x4000  (+Ve)
 *--------------------
-*        1000   0000    80  (-Ve)
+*        1000   0000  0000  0000  0x8000  (-Ve)
 *---------------------
 *
 *  40 is positive number, because bit 7 is 0.
@@ -197,11 +120,11 @@ void xtest_mcu_addw_given_a_equal_F8_than_add_08_should_get_A_equal_00_and_half_
 * overflow flag is set to 1, because sum of 2 positive number get negative result
 * negative flag is set to 1, because R7 (signing bit) is 1
 */
-void xtest_mcu_addw_given_a_equal_40_than_add_40_should_get_A_equal_E0_and_overfow_flag_1_neg_flag_1(void){
-  A = 0x40;
-  //mcu_addw(0x40);
+void test_mcu_addw_given_x_equal_40_than_add_40_should_get_A_equal_E0_and_overfow_flag_1_neg_flag_1(void){
+  X = 0x4000;
+  mcu_addw(&X, 0x4000);
 
-  TEST_ASSERT_EQUAL_INT8(0x80, A);
+  TEST_ASSERT_EQUAL_INT8(0x8000, X);
   
   TEST_ASSERT_EQUAL(1, CC.V);
   TEST_ASSERT_EQUAL(0, CC.l1);
