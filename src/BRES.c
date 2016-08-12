ddbec7cea46e8eb0b79b3b5e022e68b5b85b056c
Raydivine <radivine00@gmail.com>
2016-08-12 16:31:43 +0800

    update

--- src/BRES.c
+++ src/BRES.c
@@ -1,1 +1,63 @@
 #include "BRES.h"
+#include "MCU_Operation.h"
+#include "Description.h"
+#include <stdio.h>
+#include <stdint.h>
+#include "CPUConfig.h"
+#include "Memory.h"
+
+#define BRES(num) uint16_t longmem = GET_LONG_MEM(opcode); MEM_WRITE_BYTE(longmem, MEM_READ_BYTE(longmem) & num );
+
+
+/**
+     Binary        Hex
+    1111  1110    0xFE
+    1111  1101    0xFD    
+    1111  1011    0xFB
+    1111  0111    0xF7
+    1110  1111    0xEF
+    1101  1111    0xDF
+    1011  1111    0xBF
+    0111  1111    0x7F
+
+
+*/
+
+uint8_t bres_longmem_pos_0(uint8_t *opcode){
+  BRES(0xFE);
+  return 4;
+}
+
+uint8_t bres_longmem_pos_1(uint8_t *opcode){
+  BRES(0xFD);
+  return 4;
+}
+
+uint8_t bres_longmem_pos_2(uint8_t *opcode){
+  BRES(0xFB);
+  return 4;
+}
+
+uint8_t bres_longmem_pos_3(uint8_t *opcode){
+  BRES(0xF7);
+  return 4;
+}
+
+uint8_t bres_longmem_pos_4(uint8_t *opcode){
+  BRES(0xEF);
+  return 4;
+}
+
+uint8_t bres_longmem_pos_5(uint8_t *opcode){
+  BRES(0xDF);
+  return 4;
+}
+uint8_t bres_longmem_pos_6(uint8_t *opcode){
+  BRES(0xBF);
+  return 4;
+}
+
+uint8_t bres_longmem_pos_7(uint8_t *opcode){
+  BRES(0x7F);
+  return 4;
+}
--- src/BSET.c
+++ src/BSET.c
@@ -8,6 +8,18 @@
 
 #define BSET(num) uint16_t longmem = GET_LONG_MEM(opcode); MEM_WRITE_BYTE(longmem, MEM_READ_BYTE(longmem) | num );
 
+/**
+*          Binary       Hex
+*       0000 0001         1     bit 0
+*       0000 0010         2
+*       0000 0100         4
+*       0000 1000         8
+*       0001 0000        10
+*       0010 0000        20
+*       0100 0000        40
+*       1000 0000        80     bit 7
+*/
+
 uint8_t bset_longmem_pos_0(uint8_t *opcode){
   BSET(0x1);
   return 4;
