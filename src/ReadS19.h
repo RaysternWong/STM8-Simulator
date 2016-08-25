4ee7babe6a6dfb9592538548c52f0fdb6ad80fa4
Raydivine <raydivine00@gmail.com>
2016-08-25 15:58:59 +0800

    added comment in ReadS19.c

--- src/ReadS19.h
+++ src/ReadS19.h
@@ -1,14 +1,10 @@
 #ifndef ReadS19_H
 #define ReadS19_H
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <CException.h>
-
-//typedef enum {} ERR;
-
 
 void readS19(char *fileName);
 void S19Interpret(char *line);
